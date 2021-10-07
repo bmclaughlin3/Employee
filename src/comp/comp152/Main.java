73672a3dcbcca9df13427714e90cc277cc489a07
bmclaughlin3 <b4mclaughlin@student.bridgew.edu>
2021-10-07 11:28:58 -0400

    Added  code in main to test 3 employees calsses

--- src/comp/comp152/Main.java
+++ src/comp/comp152/Main.java
@@ -1,14 +1,25 @@
 package comp.comp152;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	Weekday deliveryDay = Weekday.Friday;
-    var picker = new Random();
-    var choice = picker.nextInt(Weekday.values().length);
-    deliveryDay = Weekday.values()[choice];
-    System.out.println("Your delivery will arrive on "+deliveryDay);
+        var allEmployees = new ArrayList<Employee>();
+        Employee newEmployee = new CommissionEmployee();
+        allEmployees.add(newEmployee);
+        newEmployee = new SalariedEmployee(55000);
+        allEmployees.add(newEmployee);
+        //add more as needed here
+
+        for (var emp : allEmployees) {
+            System.out.println("Paying a " + emp.getClass().getName() + " $" + emp.calculateBiWeeklyPay());
+//	Weekday deliveryDay = Weekday.Friday;
+//    var picker = new Random();,
+//    var choice = picker.nextInt(Weekday.values().length);
+//    deliveryDay = Weekday.values()[choice];
+//    System.out.println("Your delivery will arrive on "+deliveryDay);
+        }
     }
 }
